f20c02877dd59dcecadbe64bed98c1e4c1fa63a5
lucasclaxton1997 <lucasslclaxton@gmail.com>
2026-01-04 07:23:19 +0000

    Fix: Added Router base path and synced root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,20 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import Admin from "@/pages/Admin";
-import OneOnOneBoxing from "@/pages/services/OneOnOneBoxing";
-import GroupBoxing from "@/pages/services/GroupBoxing";
-import WilliamsAZ from "@/pages/locations/WilliamsAZ";
-import Route66Boxing from "@/pages/locations/Route66Boxing";
-import NorthernArizona from "@/pages/locations/NorthernArizona";
+import { Switch, Route, Router as WouterRouter } from "wouter"; // Ensure Router is imported
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-      {/* Service Pages */}
-      <Route path="/one-on-one-boxing" component={OneOnOneBoxing} />
-      <Route path="/group-boxing" component={GroupBoxing} />
-      {/* Location Pages */}
-      <Route path="/williams-az-boxing" component={WilliamsAZ} />
-      <Route path="/route-66-boxing" component={Route66Boxing} />
-      <Route path="/northern-arizona-boxing" component={NorthernArizona} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/canyonboxing">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+        {/* Service Pages */}
+        <Route path="/one-on-one-boxing" component={OneOnOneBoxing} />
+        <Route path="/group-boxing" component={GroupBoxing} />
+        {/* Location Pages */}
+        <Route path="/williams-az-boxing" component={WilliamsAZ} />
+        <Route path="/route-66-boxing" component={Route66Boxing} />
+        <Route path="/northern-arizona-boxing" component={NorthernArizona} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
