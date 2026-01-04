b80df53f4f1eeda721b5474e1b0f69c1f6e6132b
lucasclaxton1997 <lucasslclaxton@gmail.com>
2026-01-04 07:25:24 +0000

    Deployment: Fixed export and base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,15 @@
-import { Switch, Route, Router as WouterRouter } from "wouter"; // Ensure Router is imported
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+import Admin from "@/pages/Admin";
+import OneOnOneBoxing from "@/pages/services/OneOnOneBoxing";
+import GroupBoxing from "@/pages/services/GroupBoxing";
+import WilliamsAZ from "@/pages/locations/WilliamsAZ";
+import Route66Boxing from "@/pages/locations/Route66Boxing";
+import NorthernArizona from "@/pages/locations/NorthernArizona";
 
 function Router() {
   return (
@@ -18,3 +29,14 @@ function Router() {
     </WouterRouter>
   );
 }
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <Toaster />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
